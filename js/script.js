120a3b3a79a2bf1ece5dbba5366156a9e6cf8d91
ShilovMisha <kaynama@i.ua>
2019-09-21 09:45:57 +0300

    Optimize

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 
-var jsFlag = document.querySelector('.no-js');
-jsFlag.classList.remove('no-js');
-jsFlag.classList.add('js');
+
 
 var navToggle = document.querySelector('.main-header__toggle');
 var navMenu = document.querySelector('.main-header__list-container');
@@ -108,33 +106,120 @@ if (buttonNext && buttonPrevious) {
 	buttonPrevious.addEventListener('click', translate);
 }
 
-// buttonPrevious.addEventListener('click', function(evt){
-// 	evt.preventDefault();
 
-// 		for (var i = 0; i < reviewItems.length; i++) {
+
+
+//ForEach для IE
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fn, scope) {
+        for(var i = 0, len = this.length; i < len; ++i) {
+            fn.call(scope, this[i], i, this);
+        }
+    }
+}
+
+var lazyLoad = function(){
+	if ('IntersectionObserver' in window){
+	
+	function lazyLoadPicture(element){
+		  element.forEach(function(change) {
+		 	
 		
-// 		if (reviewItems[i].classList.contains('reviews__item--current') && i !== 0) {
-// 			reviewItems[i].classList.remove('reviews__item--current');
-// 			reviewItems[i - 1].classList.add('reviews__item--current');
-// 		}
-// 	}
-// 	translate();
-// });
-// buttonNext.addEventListener('click', function(evt){
-// 	evt.preventDefault();
-
-// 		for (var i = 0; i < reviewItems.length-1; i++) {
 		
-// 		if (reviewItems[i].classList.contains('reviews__item--current')) {
-// 			reviewItems[i].classList.remove('reviews__item--current');
-// 			console.log(i);
-// 			reviewItems[i + 1].classList.add('reviews__item--current');
-// 			break;
-// 		}
-// 	}
-// 	translate();
-// });
+    
+    if(change.isIntersecting) {
+      	//для всех элементов с dataset-src
+		if (change.target.dataset.src) {
+			change.target.src = change.target.dataset.src;
+			change.target.removeAttribute('data-src');
+		}
+		////////////////////////
+		//для всех элементов с <img dataset-src='' dataset-srcset=''> внутри
+		var picture = change.target.querySelector('img');
+		if (picture){
+				if (picture.dataset.src) {
+				picture.src = picture.dataset.src;
+				picture.removeAttribute('data-src');
+			}
+			if (picture.dataset.srcset) {
+				picture.srcset = picture.dataset.srcset;
+				picture.removeAttribute('data-srcset');
+			}
+		}
+		
+		//для всех элементов с <source dataset-srcset=''> внутри
+		var sources = change.target.querySelectorAll('source');
+		for (var j = 0; j < sources.length; j++){
+			if (sources[j]){
+				if (sources[j].dataset.srcset) {
+					sources[j].srcset = sources[j].dataset.srcset;
+					sources[j].removeAttribute('data-srcset');
+				}
+			}
+			
+		}
+    }
+  });
+	}
+
+		let options = {
+	  threshold: [0],
+	   rootMargin: '100px'
+	};
+
+	let watchMe = new IntersectionObserver(lazyLoadPicture, options);
+	//установить обсервер для picture
+	var pictures = document.querySelectorAll('picture');
+	for (var i = 0; i < pictures.length; i++) {
+		var picImg = pictures[i].querySelector('img');
+		if (picImg) {
+			if (picImg.dataset){
+				if (picImg.dataset.src){
+					watchMe.observe(pictures[i]);
+				}
+			}
+		}	
+	}
+	//установить обсервер для iframe
+	var iframes = document.querySelectorAll('iframe:not([src])');
+	if (iframes){
+		for (var i = 0; i < iframes.length; i++) {
+
+			watchMe.observe(iframes[i]);	
+		}
+	}
+}
+}
 
 
-//reviewsList.children[1].style.transform="translateX(100%)";
-//console.log(reviewsList.children[1].style.transform);
+
+//Инклюды для IE
+var body = document.querySelector("body");
+var html = document.querySelector("html");
+var scriptElement = document.createElement('script');
+if (!('IntersectionObserver' in window)){
+	var currentElement = scriptElement.cloneNode();
+	currentElement.src = "js/intersection-observer.js";
+	
+	body.appendChild(currentElement);
+
+currentElement.onload = function() {
+ 	setTimeout(lazyLoad, 10);
+};
+	
+} else lazyLoad();
+
+var loadFallback = function () {
+	if (html.classList.contains('no-picture')){
+	console.log(1);
+	var currentElement = scriptElement.cloneNode();
+	currentElement.src = "js/picturefill.min.js";
+	
+	body.appendChild(currentElement);
+	var currentElement = scriptElement.cloneNode();
+	currentElement.src = "js/pf.mutation.min.js";
+	body.appendChild(currentElement);
+	}
+}
+
+setTimeout(loadFallback, 100);	
